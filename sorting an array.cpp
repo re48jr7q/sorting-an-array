bf1b8a2ac4aa61e2ef6c0724616df289694aaddf
re48jr7q <erwin.degeus45@gmail.com>
2022-09-03 05:11:50 +0200

    <learning>

--- sorting an array.cpp
+++ sorting an array.cpp
@@ -1,8 +1,18 @@
-#include <utility>
+#include <algorithm> //std::sort
 #include <iostream>
+#include <iterator>  //std::size
 
 int main()
 {
-  
+	int array[]{ 3, 5, 1, 6, 9, 4, 2, 8, 7 };
+
+	std::sort(std::begin(array), std::end(array));
+
+	for (int i; i < static_cast<int>(std::size(array)); ++i)
+		std::cout << array << " ";
+
+	std::cout << '\n';
+
+	return 0;
 }
 
